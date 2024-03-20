b6f20eccfc90d0a6e2b999b788caadd79dc9604e
Frank Riccobono <F.Riccobono@gmail.com>
2024-03-19 21:10:06 -0400

    Grid report properly identifies things I think

--- app/src/main/java/org/ncfl/specs/CompetitionGridReport.java
+++ app/src/main/java/org/ncfl/specs/CompetitionGridReport.java
@@ -1,34 +1,39 @@
 package org.ncfl.specs;
 
+import j2html.tags.DomContent;
 import j2html.tags.specialized.TrTag;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static j2html.TagCreator.td;
-import static j2html.TagCreator.tr;
-import static j2html.TagCreator.table;
+import static j2html.TagCreator.*;
 
 public class CompetitionGridReport implements Reporter {
 
-    private record GridRow(
-        String Prelims,
-        String PlayIn,
-        String Doubles,
-        String Octos,
-        String Quarters,
-        String Semis,
-        String Finals
-    ){
-
-    }
-
     @Override
     public String process(List<Hotel> hotelList) {
-        return table()
+        return table().attr("border","1")
+            .with(
+                thead(
+                    tr(
+                        th("Venue"),
+                        th("Room"),
+                        th("Activities"),
+                        th("Prelims"),
+                        th("Play-In"),
+                        th("Doubles"),
+                        th("Octos"),
+                        th("Quarters"),
+                        th("Semis"),
+                        th("Finals"),
+                        th("???")
+                    )
+                )
+            )
             .with(
                 hotelList
                     .stream()
@@ -50,20 +55,73 @@ public class CompetitionGridReport implements Reporter {
                     AlphaNumComparator.ALPHANUM
                 )
             )
-            .map(entry -> tr(
-                td(entry.getKey().venue()),
-                td(entry.getKey().name()),
-                td(
-                    getActivities(entry.getValue())
-                )));
+            .map(entry -> {
+                final String activities = getActivities(entry.getValue());
+                    return tr(
+                        td(entry.getKey().venue()),
+                        td(entry.getKey().name()),
+                        td(
+                            activities
+                        ))
+                        .with(columns(activities));
+                }
+            );
+    }
+
+    private List<DomContent> columns(String value) {
+
+        List<String> prelims = new LinkedList<>();
+        List<String> playIn = new LinkedList<>();
+        List<String> doubles = new LinkedList<>();
+        List<String> octos = new LinkedList<>();
+        List<String> quarters = new LinkedList<>();
+        List<String> semis = new LinkedList<>();
+        List<String> finals = new LinkedList<>();
+        List<String> unknown = new LinkedList<>();
+
+        for (String activity : value.split(",")) {
+            if(activity.contains("Prelim")){
+                prelims.add(activity);
+            } else if(activity.contains("Play-In")){
+                playIn.add(activity);
+            } else if(activity.contains("Double-OctoFinals") || activity.contains("Doubles")){
+                doubles.add(activity);
+            } else if(activity.contains("Octos")){
+                octos.add(activity);
+            } else if(activity.contains("Quarters")){
+                quarters.add(activity);
+            } else if(activity.contains("Semis")){
+                semis.add(activity);
+            } else if(activity.contains("Finals")){
+                finals.add(activity);
+            } else {
+                unknown.add(activity);
+            }
+
+        }
+        return List.of(
+            td(prelims.toString()),
+            td(playIn.toString()),
+            td(doubles.toString()),
+            td(octos.toString()),
+            td(quarters.toString()),
+            td(semis.toString()),
+            td(finals.toString()),
+            td(unknown.toString())
+        );
     }
 
     private static String getActivities(List<RoomUsage> value) {
         return value
             .stream()
-            .map(RoomUsage::activity)
+            .map(usage -> {
+                if(usage.roomSet() == RoomSet.SPEECH_PRELIM || usage.roomSet() == RoomSet.DEBATE_PRELIM || usage.roomSet()==RoomSet.CONGRESS_PRELIM) {
+                    return usage.activity().replaceAll("#\\d+", "Prelim");
+                } else {
+                    return usage.activity();
+                }
+            })
             .distinct()
-            .map(activity -> activity.replaceAll("#\\d+", "Prelim"))
             .collect(Collectors.joining(","));
     }
 
